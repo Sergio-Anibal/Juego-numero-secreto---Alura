0418116749ad34342054cb98e8867ad638c5a6a5
Sergio Andres Anibal <sergioanibal17@gmail.com>
2025-01-24 22:07:22 -0500

    ciclo y contador de intentos

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,14 +1,29 @@
 //Variables
-let numeroSecreto = 8;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor");
+let numeroSecreto = 5;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
 
-console.log(numeroUsuario);
+    //Ciclo para que el juego no termine hasta que se acierte el número
+    while (numeroUsuario != numeroSecreto){
+    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor");
 
-if (numeroUsuario == numeroSecreto) {
-    //Se cumple la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else{
-    //No se cumple la condición
-    alert('Lo siento, no acertaste el número');
+    console.log(numeroUsuario);
+
+    if (numeroUsuario == numeroSecreto) {
+        //Se cumple la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+    } else{
+        
+        if(numeroUsuario > numeroSecreto){
+            alert('El número secreto es menor')
+        }else{
+            alert('El número secreto es mayor')
+        }
+        //Cuando no acierta incrementa el contador
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+        //No se cumple la condición
+       // alert('Lo siento, no acertaste el número');
+    }
 }
-
