42d9230642ed2645825fc126a5959ccf7042314d
Sergio Andres Anibal <sergioanibal17@gmail.com>
2025-01-26 17:32:12 -0500

    numero aleatorio

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,18 +1,22 @@
 //Variables
-let numeroSecreto = 5;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
+
+console.log(numeroSecreto);
 
     //Ciclo para que el juego no termine hasta que se acierte el número
     while (numeroUsuario != numeroSecreto){
-    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor");
+    let numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor"));
 
-    console.log(numeroUsuario);
+    
 
     if (numeroUsuario == numeroSecreto) {
         //Se cumple la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+        break;
     } else{
         
         if(numeroUsuario > numeroSecreto){
@@ -21,8 +25,14 @@ let palabraVeces = 'vez';
             alert('El número secreto es mayor')
         }
         //Cuando no acierta incrementa el contador
-        intentos = intentos + 1;
-        palabraVeces = 'veces';
+        //intentos = intentos + 1;
+        intentos ++
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos){
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
         //No se cumple la condición
        // alert('Lo siento, no acertaste el número');
     }
